8ef5c74d7ffaffffc0933cfa9683b786638eea69
pyratu <paul_pyratu@yahoo.com>
2023-08-02 18:44:58 +0300

    project

---
+++ Morse code text/DictionaryData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DictionaryData
+{
+    public static Dictionary<string, string> EncryptDictionary()
+    {
+        var dictionary = new Dictionary<string, string>();
+        dictionary.Add(" ", " ");
+        dictionary.Add("A", ".-");
+        dictionary.Add("B", "-...");
+        dictionary.Add("C", "-.-.");
+        dictionary.Add("D", "-..");
+        dictionary.Add("E", ".");
+        dictionary.Add("F", "..-.");
+        dictionary.Add("G", "--.");
+        dictionary.Add("H", "....");
+        dictionary.Add("I", "..");
+        dictionary.Add("J", ".---");
+        dictionary.Add("K", "-.-");
+        dictionary.Add("L", ".-..");
+        dictionary.Add("M", "--");
+        dictionary.Add("N", "-.");
+        dictionary.Add("O", "---");
+        dictionary.Add("P", ".--.");
+        dictionary.Add("Q", "--.-");
+        dictionary.Add("R", ".-.");
+        dictionary.Add("S", "...");
+        dictionary.Add("T", "-");
+        dictionary.Add("U", "..-");
+        dictionary.Add("V", "...-");
+        dictionary.Add("W", ".--");
+        dictionary.Add("X", "-..-");
+        dictionary.Add("Y", "-.--");
+        dictionary.Add("Z", "--..");
+
+        return dictionary;
+    }
+    public static Dictionary<string, string> DecryptDictionary()
+    {
+        var dictionary = new Dictionary<string, string>();
+        dictionary.Add("", "");
+        dictionary.Add(" ", "");
+        dictionary.Add("/", " ");
+        dictionary.Add(".-", "A");
+        dictionary.Add("-...", "B");
+        dictionary.Add("-.-.", "C");
+        dictionary.Add("-..", "D");
+        dictionary.Add(".", "E");
+        dictionary.Add("..-.", "F");
+        dictionary.Add("--.", "G");
+        dictionary.Add("....", "H");
+        dictionary.Add("..", "I");
+        dictionary.Add(".---", "J");
+        dictionary.Add("-.-", "K");
+        dictionary.Add(".-..", "L");
+        dictionary.Add("--", "M");
+        dictionary.Add("-.", "N");
+        dictionary.Add("---", "O");
+        dictionary.Add(".--.", "P");
+        dictionary.Add("--.-", "Q");
+        dictionary.Add(".-.", "R");
+        dictionary.Add("...", "S");
+        dictionary.Add("-", "T");
+        dictionary.Add("..-", "U");
+        dictionary.Add("...-", "V");
+        dictionary.Add(".--", "W");
+        dictionary.Add("-..-", "X");
+        dictionary.Add("-.--", "Y");
+        dictionary.Add("--..", "Z");
+        return dictionary;
+    }
+}
+
---
+++ Morse code text/Program.cs
@@ -0,0 +1,69 @@
+﻿string text;
+string morse = "";
+here:
+Console.WriteLine("To decrypt press 1");
+Console.WriteLine("To encrypt press 2");
+Console.Write("Choice=");
+text=Console.ReadLine();
+
+if (text == "1")
+{
+    Dictionary<string, string> dictionary = DictionaryData.DecryptDictionary();
+    text = Console.ReadLine();
+    decrypt(text,dictionary);
+}
+
+else if (text == "2")
+{
+    Dictionary<string, string> dictionary = DictionaryData.EncryptDictionary();
+    text = Console.ReadLine().ToUpper();
+    encrypt(text,dictionary);
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("INVALID CHOICE, TRY AGAIN");
+    Console.WriteLine(" ");
+  
+    goto here;
+}
+
+Console.WriteLine(morse);
+
+string decrypt(string input,Dictionary<string,string> dictionary)
+{
+    string part=String.Empty;
+    
+    for(int i=0;i<input.Length;i++)
+    {
+        if(input[i] =='.'|input[i]=='-')
+        {
+            part=part+input.Substring(i,1);
+        }
+        if(input[i]==' ')
+        {
+            morse = morse + dictionary[part.ToString()];
+            part = String.Empty;
+        }
+        if(input[i]=='/')
+        {
+            morse = morse + " ";
+        }
+    }
+    morse = morse + dictionary[part.ToString()] + " ";
+    return morse;
+}
+
+string encrypt(string input,Dictionary<string,string> dictionary)
+{
+    foreach (var letter in input)
+    {
+        if (letter == ' ')
+        {
+            morse = morse + '/' + " ";
+        }
+        else
+            morse = morse + dictionary[letter.ToString()] + " ";
+    }
+    return morse;
+}
